6fc5df63758fd6a2c8fbc7e62983e9a0a3292cc8
koe <k.oe@l-owcal.com>
2024-02-09 01:14:09 +0900

    js追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 document.querySelector('#plus-item-box').addEventListener('click', () => {
     const newForm = document.createElement('input');
     newForm.type = 'text';
+
+    document.querySelector('div').appendChild(newLabel);
