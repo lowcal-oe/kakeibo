6c814bb1b92afd551377d5fe0b2c88f0ad083dcf
koe <k.oe@l-owcal.com>
2024-02-09 01:19:58 +0900

    id属性の付与とjs修正

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ document.querySelector('#plus-item-box').addEventListener('click', () => {
     const newForm = document.createElement('input');
     newForm.type = 'text';
 
-    document.querySelector('div').appendChild(newLabel);
+    document.querySelector('#item-price-box').appendChild(newForm);
