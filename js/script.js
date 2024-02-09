6aeadcfd533f761883f76a884abc4cb22c907ace
koe <k.oe@l-owcal.com>
2024-02-09 14:40:43 +0900

    再修正

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.querySelector('#plus-item-box').addEventListener('click', () => {
     const inputList = document.querySelectorAll('#item-price-box input');
 
     inputList.forEach(input => {
-        const newForm = document.createElement('#item-price-box input')
+        const newForm = document.createElement('input')
         document.querySelector('#item-price-box').appendChild(newForm);
     });
 });
