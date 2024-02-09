71f28d5620cbde12d80f8602f39c41200a38a4d6
koe <k.oe@l-owcal.com>
2024-02-09 16:28:46 +0900

    index修正とjsで金額の合計

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 document.querySelector('#plus-item-box').addEventListener('click', () => {
-    const itemPriceBox = document.querySelector('#item-price-box'); // #item-price-box 要素を取得し、itemPriceBox 変数に格納
+    const itemPriceBox = document.querySelector('#item-price-box');
 
     for (let i = 0; i < 2; i++) {
         const newForm = document.createElement('input');
-        itemPriceBox.appendChild(newForm); // itemPriceBox 変数を使用してテキストボックスを追加
+        itemPriceBox.appendChild(newForm);
     }
 });
+
+function calculateTotal() {
+    const amount1 = parseFloat(document.getElementById('price-name').value) || 0;
+    const amount2 = parseFloat(document.getElementById('price-name').value) || 0;
+    const amount3 = parseFloat(document.getElementById('price-name').value) || 0;
+
+    const totalAmount = amount1 + amount2 + amount3;
+
+    document.getElementById('totalAmount').innerText = "小計：" + totalAmount;
+}
