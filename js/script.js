4ce23ed8ecfef1888f999eafd17a2d64a3255c73
koe <k.oe@l-owcal.com>
2024-02-09 14:48:58 +0900

    再修正

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.querySelector('#plus-item-box').addEventListener('click', () => {
-    const inputList = document.querySelectorAll('#item-price-box input');
+    const itemPriceBox = document.querySelector('#item-price-box'); // #item-price-box 要素を取得し、itemPriceBox 変数に格納
 
-    inputList.forEach(input => {
-        const newForm = document.createElement('input')
-        document.querySelector('#item-price-box').appendChild(newForm);
-    });
+    for (let i = 0; i < 2; i++) {
+        const newForm = document.createElement('input');
+        itemPriceBox.appendChild(newForm); // itemPriceBox 変数を使用してテキストボックスを追加
+    }
 });
