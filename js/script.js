02d9a6277d31b0c8c384bde5d0d25ebfb6da06af
koe <k.oe@l-owcal.com>
2024-02-09 11:39:16 +0900

    Revert "追加ボタン押下時にボックスを1つ追加"
    This reverts commit 08fe09c45afd26ed218160010dd34f22062b0690.
    
    間違えたのでRevertした

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-// 本日の日付を取得
-const today = new Date();
-
-// 追加ボタン
 document.querySelector('#plus-item-box').addEventListener('click', () => {
-    const newForm = document.createElement('input') + ('input');
+    const newForm = document.createElement('input');
     newForm.type = 'text';
 
     document.querySelector('#item-price-box').appendChild(newForm);
