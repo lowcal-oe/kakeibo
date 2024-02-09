39def47053176d8cb24d5150fa8b9c73ecdced7a
koe <k.oe@l-owcal.com>
2024-02-09 12:22:11 +0900

    エレメントをクリエイトするではなく、クローンさせる

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.querySelector('#plus-item-box').addEventListener('click', () => {
-    const newForm = document.createElement('input');
+    const newForm = document.querySelector('#item-price-box input').cloneNode(true);
     newForm.type = 'text';
 
     document.querySelector('#item-price-box').appendChild(newForm);
