08fe09c45afd26ed218160010dd34f22062b0690
koe <k.oe@l-owcal.com>
2024-02-09 11:30:45 +0900

    追加ボタン押下時にボックスを1つ追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+// 本日の日付を取得
+const today = new Date();
+
+// 追加ボタン
 document.querySelector('#plus-item-box').addEventListener('click', () => {
-    const newForm = document.createElement('input');
+    const newForm = document.createElement('input') + ('input');
     newForm.type = 'text';
 
     document.querySelector('#item-price-box').appendChild(newForm);
