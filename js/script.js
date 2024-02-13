43c663d2e0a699718d908cb4a1fe893716de8fe6
koe <k.oe@l-owcal.com>
2024-02-13 12:02:21 +0900

    イベントリスナーの書き換え、新しいdiv要素とinput要素の要素の作成

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
+// 追加ボタンをクリックするとテキストボックスを増やす
+// document.querySelector('.plus-item-box').addEventListener('click', () => {
+//     const itemPriceBox = document.querySelector('.item-price-box');
+
+//     for (let i = 0; i < 2; i++) {
+//         const newForm = document.createElement('input');
+//         newForm.type = 'number';
+//         newForm.name = 'price-name';
+//         itemPriceBox.appendChild(newForm);
+//     }
+// });
+
 document.querySelector('.plus-item-box').addEventListener('click', () => {
     const itemPriceBox = document.querySelector('.item-price-box');
+    // itemPriceBox.textContent = '円:';
 
-    for (let i = 0; i < 2; i++) {
-        const newForm = document.createElement('input');
-        newForm.type = 'number';
-        newForm.name = 'price-name';
-        itemPriceBox.appendChild(newForm);
-    }
+    // for (let i = 0; i < 2; i++) {
+    const newForm = document.createElement('div');
+    newForm.classList.add('item-price-box');
+
+    const itemNameInput = document.createElement('input');
+    itemNameInput.type = 'text';
+    itemNameInput.name = 'item-name';
+
+    const priceNameInput = document.createElement('input');
+    priceNameInput.type = 'number';
+    priceNameInput.name = 'price-name';
+
+    newForm.appendChild(itemNameInput);
+    newForm.appendChild(priceNameInput);
+
+    container.appendChild(newForm);
+    // }
 });
+
 
 function calculateTotal() {
     // 金額のテキストボックスをすべて取得
