373937e63face83e4e8e38a3d1c976f75b641fd1
koe <k.oe@l-owcal.com>
2024-02-16 18:12:13 +0900

    formのメソッド属性の変更、labelのfor属性の変更、confirm.phpの作成

---
+++ contact/confirm.php
@@ -0,0 +1,4 @@
+<form action="/kakeibo/contact/thanks.html" method="post">
+<button><a href="/kakeibo/contact/index.html">戻る</a></button>
+<button><a href="/kakeibo/contact/thanks.html">送信する</a></button>
+</form>
